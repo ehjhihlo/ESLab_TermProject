361d51ffab4931efb6a5c523874fd9302432f02e
En-Jhih Lo <75471051+ehjhihlo@users.noreply.github.com>
2023-11-15 21:40:05 +0800

    Update picamera.py

--- pi_camera/picamera.py
+++ pi_camera/picamera.py
@@ -1,66 +1,94 @@
-from flask import Flask, render_template_string, Response
-from io import BufferedIOBase
+# Web streaming example
+# Source code from the official PiCamera package
+# http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
+
+import io
+import picamera
+import logging
+import socketserver
 from threading import Condition
-from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder, Quality
-from picamera2.outputs import FileOutput
-from libcamera import controls, Transform
+from http import server
 
-template = '''
-    <!DOCTYPE html>
-    <html lang="en">
-        <body>
-            <img src="{{ url_for('video_stream') }}" width="100%">
-        </body>
-    </html>
-    '''
+PAGE="""\
+<html>
+<head>
+<title>Raspberry Pi - Surveillance Camera</title>
+</head>
+<body>
+<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
+<center><img src="stream.mjpg" width="640" height="480"></center>
+</body>
+</html>
+"""
 
-app = Flask(__name__)
-
-
-class StreamingOutput(BufferedIOBase):
+class StreamingOutput(object):
     def __init__(self):
         self.frame = None
+        self.buffer = io.BytesIO()
         self.condition = Condition()
 
     def write(self, buf):
-        with self.condition:
-            self.frame = buf
-            self.condition.notify_all()
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
 
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
 
-output = StreamingOutput()
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
 
-
-def gen_frames():
-    while True:
-        with output.condition:
-            output.condition.wait()
-            frame = output.frame
-
-        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route("/", methods=['GET'])
-def get_stream_html():
-    return render_template_string(template)
-
-
-@app.route('/api/stream')
-def video_stream():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-if __name__ == "__main__":
-    cam = Picamera2()
-    config = cam.create_video_configuration(
-        {'size': (1920, 1080), 'format': 'XBGR8888'},
-        transform=Transform(vflip=1),
-        controls={'NoiseReductionMode': controls.draft.NoiseReductionModeEnum.HighQuality, 'Sharpness': 1.5}
-    )
-    cam.configure(config)
-    cam.start_recording(JpegEncoder(), FileOutput(output), Quality.VERY_HIGH)
-
-    app.run(host='0.0.0.0')
-
-    cam.stop()
+with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+    output = StreamingOutput()
+    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+    #camera.rotation = 90
+    camera.start_recording(output, format='mjpeg')
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
