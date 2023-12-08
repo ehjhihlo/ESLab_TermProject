963a294d8e55a2cdd621210a50fa4b3f91298476
En-Jhih Lo <75471051+ehjhihlo@users.noreply.github.com>
2023-12-08 16:01:28 +0800

    Add files via upload

---
+++ socketio/server.py
@@ -0,0 +1,43 @@
+import socketio
+import eventlet
+import numpy as np
+import json
+import cv2
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+count = 0
+
+@sio.event
+def connect(sid, environ):
+    print('Client connected:', sid)
+
+@sio.event
+def disconnect(sid):
+    print('Client disconnected:', sid)
+
+# @sio.event
+# def image(sid, data):
+#     print(f'Received image from rpi: {data}')
+#     img = np.fromstring(data, dtype=int)
+#     print(img)
+#     img = img.reshape((160, 160))
+#     print(img)
+
+@sio.event
+def image(sid, data):
+    global count
+    print(f'Received image from rpi!!!')
+    img = np.array(json.loads(data))
+    count += 1
+    # print(img)
+    # print(img.shape)
+    cv2.imwrite(f"./images/img_{count}.png", img)
+    print(f'image {count} saved!!')
+
+
+if __name__ == '__main__':
+    port = 3000
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', port)), app)
+    # print(f'Server is running on port {port}')
