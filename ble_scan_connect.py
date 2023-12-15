1ef06330d93aef1397f5eaf71ef95a32270a0154
En-Jhih Lo <75471051+ehjhihlo@users.noreply.github.com>
2023-12-15 18:58:20 +0800

    Update ble_scan_connect.py

--- ble_scan_connect.py
+++ ble_scan_connect.py
@@ -18,8 +18,10 @@ import base64
 
 
 global dev
+#global alert
 
 flag = 0
+alert="false"
 import socketio
 
 sio = socketio.Client()
@@ -34,13 +36,12 @@ def disconnect():
 
 @sio.event
 def receive(data):
+    global alert
     print("result:",data)
-    if data==True:
-        ch = dev.getCharacteristics(uuid=UUID(0xA001))[0]
-        ch.write(b"\x01")
+    alert = data
 
 #server_address = "http://192.168.11.12:3000"
-server_address = "http://192.168.0.186:3000"
+server_address = "http://192.168.0.138:3000"
 sio.connect(server_address)
 
 
@@ -66,7 +67,7 @@ class NewDelegate(btle.DefaultDelegate):
             sio.emit("image", img_json)
         else:
             flag = 0
-        print('flag = ', flag)
+        #print('flag = ', flag)
     
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -98,24 +99,6 @@ for dev in devices:
 assert(num!=-1)
 
 
-# number = input('Enter your device number: ')
-# print ('Device', number)
-# num = int(number)
-# print (addr[num])
-#
-
-#Instantiate and configure picamera 
-#camera = PiCamera()
-#camera.resolution = (640, 480)
-#camera.framerate = 32
-#raw_capture = PiRGBArray(camera, size=(640, 480))
-
-#let camera module warm up 
-#time.sleep(0.1)
-
-# define an OpenCV window to display  video
-#cv2.namedWindow("Frame")
-
 print ("Connecting...")
 dev = Peripheral(addr[num], 'random')
 #
@@ -129,42 +112,17 @@ notify_handle = notify.getHandle() + 1
 dev.writeCharacteristic(notify_handle, setup_data, withResponse=True)
 
 while True:
-#for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
-    #for ch in dev.getCharacteristics(uuid=UUID(0xfff4)):
-        # ch.write('52399254'.encode('utf-8'))
-    #image = frame.array
-    #cv2.imshow("Frame", image)
-    #key = cv2.waitKey(1) & 0xFF
-    #raw_capture.truncate(0)
-    #if 'q' is pressed, close OpenCV window and end video
-    #if key != ord('q'):
-        #pass
-    #else:
-        #cv2.destroyAllWindows()
-        #break
-    
     dev.setDelegate(NewDelegate())
-
-    print("writing done")
+    print(alert)
+    if alert=="true":
+        ch = dev.getCharacteristics(uuid=UUID(0xA001))[0]
+        ch.write(b"\x01")
+        print("hewlqwhrejlqwh")
+        alert = "false"
+    #print("writing done")
     if dev.waitForNotifications(1.0):
-        #ch.write('44444'.encode('utf-8'))
-        # handleNotification() was called
-        print("write notify")
-        #print(flag)
-        #if flag == 1:
-            #cv2.imwrite("image.png", image)
+        print(" ")
 
-        # pseudo for testing write characteristic to server
-        # ch = dev.getCharacteristics(uuid=UUID(0xA001))[0]
-        # # print(ch)
-        # ch.write(b"\x01")
-        # #print('write 1 to stm')
-        
-        #ch = dev.getCharacteristics(uuid=UUID(0xfff4))[0]
-        #if (ch.supportsRead()):
-        #print(ch.read())
     print("waiting")
 
 sio.disconnect()
-#finally:
-    #dev.disconnect() 
